21f1947355ed6fc03bd87ecd41a6f6d8d5c17eda
ravindu <ravindulakshitha20032411@gmail.com>
2026-07-21 19:23:11 +0530

    add lordbalancer

--- Company/order/src/main/java/com/example/orderservice/service/OrderService.java
+++ Company/order/src/main/java/com/example/orderservice/service/OrderService.java
@@ -31,7 +31,7 @@ public class OrderService {
         Integer id = Integer.valueOf(orderDto.getId());
         try{
             InventoryDto data = webClient.get()
-                    .uri("http://localhost:8002/api/inventory/{id}", id)
+                    .uri("http://inventory-service/api/inventory/{id}", id)
                     .retrieve()
                     .bodyToMono(InventoryDto.class)
                     .block();
